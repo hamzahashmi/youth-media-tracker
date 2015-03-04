96e8d5fca101c2fb999725f8d8dca78d89bf4ba3
saasbook <saasbook@saasbook.(none)>
2015-03-04 18:41:13 -0400

    Scaffolding and features

---
+++ YMT/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YMT::Application.config.secret_token = '5343f6baed5b0ff98b16dcad37da21ad6f3ee6feeae7c78cc85c9455026dbfd9a6e4ab24dcaa238b4c42c74136a753fd1ca11ea3b2f4b9c3645fab9c1fc29d00'
