63392d43e5073afe5049ada94a8a31fb2c51644a
Kamel <kdabwan@berkeley.edu>
2015-03-13 09:55:02 -0700

    fixing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: "youth.media.tracker@gmail.com",
+    password: "groupcs169"
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
