c70c5d3752fc3595508d7a39ba3e5990a8f6ef43
saasbook <jonathan.ting@berkeley.edu>
2015-04-30 07:40:49 -0300

    fixed migration issue

--- spec/factories/pitches.rb
+++ spec/factories/pitches.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :pitch do
       name "testName"
-      #Doesn't quite work, have to actually replace
-	  media_type FactoryGirl.create(:media_type)
-	  category FactoryGirl.create(:category)
+	  media_type 
+	  category 
 	  description "testText"
 	  user
   end
