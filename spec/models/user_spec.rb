5821c9d375a85dd50d55c7ad7cfa4c923ba794a1
Kamel <kdabwan@berkeley.edu>
2015-04-30 20:13:06 -0700

    testing user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,12 @@ describe User do
       FactoryGirl.create(:user, :email => "his@email.com").should be_valid
       FactoryGirl.create(:user, :email => "her@email.com").should be_valid
     end
+
+    it 'should raise error' do
+     #
+      expect { FactoryGirl.create(:user, :email => "his@email.com" ,:address_zip_code => 12344)}.to raise_error
+      #user.errors.should have_key(:address_zip_code)
+    end
     
   end
 
