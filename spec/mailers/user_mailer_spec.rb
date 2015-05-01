11025064444dc4c2ce50a27fc47ec2e2dad7f1b4
Kamel <kdabwan@berkeley.edu>
2015-04-30 19:56:23 -0700

    testing mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should send email" do
+	user = FactoryGirl.create(:user ,:email => "ex@ample.com")
+	pitch = FactoryGirl.create(:pitch, :user => user)
+  	email = UserMailer.submit_final_work(pitch).deliver_now
+  	ActionMailer::Base.deliveries.should_not be_empty
+  	email.to.first.should eq("ex@ample.com")
+  	email.body.to_s.should have_content("Congratulation")
+  end
+
 end
