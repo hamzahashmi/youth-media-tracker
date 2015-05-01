ab402c772d4f37d0284d6c5ebfbb6ee39c44bc13
Kamel <kdabwan@berkeley.edu>
2015-04-30 20:51:48 -0700

    testing comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,3 +1,18 @@
 RSpec.describe CommentsController do
-  render_views
+  before(:all) do
+    @user = FactoryGirl.create(:user)
+    @pitch = FactoryGirl.create(:pitch, :user => @user)
+  end
+    it "blocks unauthenticated access" do
+    get :new, :pitch_id => @pitch.id
+    response.should redirect_to(new_user_session_path)
+  end
+
+  it "allows authenticated access" do
+    sign_in @user
+
+    get :new, :pitch_id => @pitch.id
+
+    response.should be_success
+  end
 end
