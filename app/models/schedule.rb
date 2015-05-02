a466a8d24d115d2548bfae73e532d23c92abceb6
saasbook <jonathan.ting@berkeley.edu>
2015-05-02 03:19:46 -0300

    okay tests should pass this time

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,29 +1,43 @@
 class Schedule < ActiveRecord::Base
 	validates :iteration_start, :iteration_end, :submission_pick, :presence => true
 
-	#If not seeded properly
-	if Schedule.all.length == 0
-		iteration_start = DateTime.civil(2015, 1, 5, 12, 0, 0, Rational(-8, 24))
-		iteration_end = DateTime.civil(2015, 1, 12, 12, 0, 0, Rational(-8, 24))
-		submission_pick = DateTime.civil(2015, 1, 13, 17, 0, 0, Rational(-8, 24))
-		Schedule.delete_all
-		Schedule.create!(:iteration_start => iteration_start,
-		                 :iteration_end => iteration_end,
-		                 :submission_pick => submission_pick)
-	end
-
 	@entry = Schedule.all[0]
 
 	def self.update
 		@entry = Schedule.all[0]
+		if Schedule.all.length == 0
+			Schedule.create
+			Schedule.update
+		else
+			return Schedule.all[0]
+		end
 	end
 
 	def self.entry
 		@entry = Schedule.all[0]
+		if Schedule.all.length == 0
+			Schedule.create
+			Schedule.update
+		else
+			return Schedule.all[0]
+		end
+	end
+
+	def self.create
+		#If not seeded properly
+		if Schedule.all.length == 0
+			iteration_start = DateTime.civil(2015, 1, 5, 12, 0, 0, Rational(-8, 24))
+			iteration_end = DateTime.civil(2015, 1, 12, 12, 0, 0, Rational(-8, 24))
+			submission_pick = DateTime.civil(2015, 1, 13, 17, 0, 0, Rational(-8, 24))
+			Schedule.delete_all
+			Schedule.create!(:iteration_start => iteration_start,
+			                 :iteration_end => iteration_end,
+			                 :submission_pick => submission_pick)
+		end
 	end
 
 	def self.iter_start_hour
-		@hour = @entry.iteration_start.time.hour % 12
+		@hour = entry.iteration_start.time.hour % 12
 		if @hour == 0
 			@hour = 12
 		end
@@ -31,19 +45,19 @@ class Schedule < ActiveRecord::Base
 	end
 
 	def self.iter_start_min
-		@entry.iteration_start.time.min
+		entry.iteration_start.time.min
 	end
 
 	def self.iter_start_ampm
 		@time = "am"
-		if @entry.iteration_start.time.hour >= 12
+		if entry.iteration_start.time.hour >= 12
 			@time = "pm"
 		end
 		@time
 	end
 
 	def self.iter_end_hour
-		@hour = @entry.iteration_end.time.hour % 12
+		@hour = entry.iteration_end.time.hour % 12
 		if @hour == 0
 			@hour = 12
 		end
@@ -51,19 +65,19 @@ class Schedule < ActiveRecord::Base
 	end
 
 	def self.iter_end_min
-		@entry.iteration_end.time.min
+		entry.iteration_end.time.min
 	end
 
 	def self.iter_end_ampm
 		@time = "am"
-		if @entry.iteration_end.time.hour >= 12
+		if entry.iteration_end.time.hour >= 12
 			@time = "pm"
 		end
 		@time
 	end
 
 	def self.sub_pick_hour
-		@hour = @entry.submission_pick.time.hour % 12
+		@hour = entry.submission_pick.time.hour % 12
 		if @hour == 0
 			@hour = 12
 		end
@@ -71,19 +85,19 @@ class Schedule < ActiveRecord::Base
 	end
 
 	def self.sub_pick_min
-		@entry.submission_pick.time.min
+		entry.submission_pick.time.min
 	end
 
 	def self.sub_pick_ampm
 		@time = "am"
-		if @entry.submission_pick.time.hour >= 12
+		if entry.submission_pick.time.hour >= 12
 			@time = "pm"
 		end
 		@time
 	end
 
 	def self.iter_start
-		@start = @entry.iteration_start
+		@start = entry.iteration_start
 		while (@start - DateTime.current) < 0
 			@start = @start.advance :weeks => 1
 		end
@@ -91,7 +105,7 @@ class Schedule < ActiveRecord::Base
 	end
 
 	def self.iter_end
-		@end = @entry.iteration_end
+		@end = entry.iteration_end
 		while @end - DateTime.current < 0
 			@end = @end.advance :weeks => 1
 		end
